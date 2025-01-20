b0e7b98b01df78eb4ce1e0ada238cdd5a4616ae8
Jonazzzz <bombastickj@gmail.com>
2025-01-20 12:54:40 +0100

    feat(playground): Add WebSocket integration and reconnection UI
    - Integrate WebSocket functionality with new ping and disconnect buttons in the template.
    - Add EasyWSTimeout component to handle WebSocket reconnection with user interface.

--- playground/app.vue
+++ playground/app.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     Nuxt module playground!
+    <button @click="sendPing">
+      Ping!
+    </button>
+    <button @click="disconnect">
+      Disconnect
+    </button>
+
+    <EasyWSTimeout />
   </div>
 </template>
 
 <script setup lang="ts">
+const { send, disconnect } = useEasyWS()
+
+function sendPing() {
+  send('ping', { timestamp: Date.now() })
+}
 onMounted(() => {
-  useEasyWS().send('ping', { timestamp: Date.now() })
+  sendPing()
   // useNuxtApp().$easyWS.send('chat/send', {})
 })
 </script>
---
+++ playground/components/EasyWSTimeout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    v-if="state.isReconnecting"
+    class="easy-ws-timeout-wrapper"
+  >
+    <div class="card">
+      <div
+        v-if="state.reconnectCountdown"
+        class="message"
+      >
+        <p>{{ state.lastError }}</p>
+        <p>
+          Connection lost. Reconnecting in {{ state.reconnectCountdown }} second<span v-if="state.reconnectCountdown !== 1">s</span>...
+        </p>
+      </div>
+      <div
+        v-else
+        class="message"
+      >
+        <p>
+          {{ maxReconnectAttemptsReached && isDisconnected ? 'Connection failed' : 'Reconnecting...' }}
+        </p>
+      </div>
+
+      <button
+        v-if="isDisconnected"
+        @click="forceReconnect"
+      >
+        Reconnect Now
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { state, connectionStatus, maxReconnectAttemptsReached, forceReconnect } = useEasyWS()
+const isDisconnected = computed(() => connectionStatus.value === 'disconnected')
+</script>
+
+<style>
+.easy-ws-timeout-wrapper {
+  position: fixed;
+  inset: 0;
+
+  display: grid;
+  justify-items: center;
+  align-items: center;
+}
+.easy-ws-timeout-wrapper .card {
+  width: min(100%, 400px);
+  background-color: lightgray;
+
+  padding: 10px;
+}
+</style>

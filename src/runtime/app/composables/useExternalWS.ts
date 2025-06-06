3a8d901a4de449c71ac17a78bc4c4b9c8ed730a5
Jonazzzz <bombastickj@gmail.com>
2025-06-06 11:37:13 +0200

    refactor: remove EasyWSExternalRoutes dependency inside useExternalWS

--- src/runtime/app/composables/useExternalWS.ts
+++ src/runtime/app/composables/useExternalWS.ts
@@ -1,7 +1,6 @@
 import { useNuxtApp, type NuxtApp } from '#app'
-import type { EasyWSExternalRoutes } from '#nuxt-easy-websocket/routes'
 
-export function useExternalWS<T extends keyof EasyWSExternalRoutes>(name: T): NuxtApp['$easyWS']['external'][T] {
+export function useExternalWS<T extends keyof NuxtApp['$easyWS']['external']>(name: T): NuxtApp['$easyWS']['external'][T] {
   const nuxtApp = useNuxtApp()
   return nuxtApp.$easyWS.external[name]!
 }

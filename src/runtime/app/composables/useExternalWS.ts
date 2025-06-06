d7a211e81f20cb282e4471bf29f56770c9daa9e9
Jonazzzz <bombastickj@gmail.com>
2025-06-06 11:32:05 +0200

    fix: useExternalWS return type definition

--- src/runtime/app/composables/useExternalWS.ts
+++ src/runtime/app/composables/useExternalWS.ts
@@ -1,7 +1,7 @@
-import { useNuxtApp } from '#app'
+import { useNuxtApp, type NuxtApp } from '#app'
 import type { EasyWSExternalRoutes } from '#nuxt-easy-websocket/routes'
 
-export function useExternalWS<T extends keyof EasyWSExternalRoutes>(name: T) {
+export function useExternalWS<T extends keyof EasyWSExternalRoutes>(name: T): NuxtApp['$easyWS']['external'][T] {
   const nuxtApp = useNuxtApp()
   return nuxtApp.$easyWS.external[name]!
 }
